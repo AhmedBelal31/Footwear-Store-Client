0cae589982def4b41eefdbdcc69df15f0810fcc7
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-06-01 06:57:24 +0300

    Create Login Screen UI

--- lib/presentation/screens/login_screen.dart
+++ lib/presentation/screens/login_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:footwear_store_client/core/utils/styles.dart';
+
+import '../widgets/custom_text_field.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -6,8 +9,42 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login Screen !_!'),
+      body: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.blueGrey[50],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome Back !',
+              style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: AppStyles.kPrimaryColor),
+            ),
+            const SizedBox(height: 20),
+            const CustomTextField(
+              hintText: 'Mobile Number',
+              prefixIcon: Icons.phone_android,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: AppStyles.kPrimaryColor,
+              ),
+              onPressed: () {},
+              child: const Text('  Login  '),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text('Register new account ?'),
+            ),
+          ],
+        ),
       ),
     );
   }
