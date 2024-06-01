2f09c630b58897393da0c50a392f75b77f9d69d6
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-06-01 06:44:26 +0300

    Add Bloc Observer

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:footwear_store_client/presentation/screens/login_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'core/utils/bloc_observer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  Bloc.observer = MyBlocObserver();
   runApp(const FootWearStoreClient());
 }
 
