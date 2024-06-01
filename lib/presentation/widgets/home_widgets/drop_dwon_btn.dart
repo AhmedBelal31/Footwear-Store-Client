d9c3b9ae7736d6ef3eac83f6b9c6fb88a2307342
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-06-01 12:45:37 +0300

    Custom Drop Down Buttons

--- lib/presentation/widgets/home_widgets/drop_dwon_btn.dart
+++ lib/presentation/widgets/home_widgets/drop_dwon_btn.dart
@@ -2,56 +2,61 @@ import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 
 class CustomDropDownBtn extends StatelessWidget {
-  const CustomDropDownBtn({super.key});
+
+  final String selectedItemText;
+  final Function(String?)? onValueChanged;
+
+  const CustomDropDownBtn(
+      {super.key,
+      required this.selectedItemText,
+      this.onValueChanged});
 
   @override
   Widget build(BuildContext context) {
     final List<String> items = [
-      'Rs:Low to High',
-      'Rs: High to Low',
-
+      'Low to High',
+      'High to Low',
     ];
-    String? selectedValue;
-    return Center(
-      child: DropdownButtonHideUnderline(
-        child: DropdownButton2<String>(
-          isExpanded: true,
-          hint: Text(
-            'Sort Items',
-            style: TextStyle(
-              fontSize: 14,
-              color: Theme.of(context).hintColor,
-            ),
-          ),
-          items: items
-              .map((String item) => DropdownMenuItem<String>(
-            value: item,
-            child: Text(
-              item,
-              style: const TextStyle(
+    return Card(
+      child: Center(
+        child: DropdownButtonHideUnderline(
+          child: DropdownButton2<String>(
+            isExpanded: true,
+            hint: Text(
+              selectedItemText,
+              style: TextStyle(
                 fontSize: 14,
+                color: Theme.of(context).hintColor,
               ),
             ),
-          ))
-              .toList(),
-          value: selectedValue,
-          onChanged: (String? value) {
-            // setState(() {
-            //   selectedValue = value;
-            // });
-          },
-          buttonStyleData: const ButtonStyleData(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            height: 40,
-            width: 140,
-          ),
-          menuItemStyleData: const MenuItemStyleData(
-            height: 40,
+            items: items
+                .map((String item) => DropdownMenuItem<String>(
+                      value: item,
+                      child: Text(
+                        item,
+                        style: const TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                    ))
+                .toList(),
+            //value: items.contains(selectedValue) ? selectedValue : null,
+            onChanged: (String? value) {
+              if (onValueChanged != null) {
+                onValueChanged!(value);
+              }
+            },
+            buttonStyleData: const ButtonStyleData(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              height: 40,
+              width: 140,
+            ),
+            menuItemStyleData: const MenuItemStyleData(
+              height: 40,
+            ),
           ),
         ),
       ),
     );
   }
 }
-
-
