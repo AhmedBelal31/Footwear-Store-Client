c4b54cb78865a91d28cd437722f27f2b19a76a24
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-06-04 12:28:52 +0300

    Custom Decoration For Selected Category

--- lib/presentation/widgets/home_widgets/categories_listview.dart
+++ lib/presentation/widgets/home_widgets/categories_listview.dart
@@ -3,11 +3,19 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:footwear_store_client/data/models/product_category.dart';
 import 'package:footwear_store_client/presentation/controller/products_cubit.dart';
 
-class CategoriesListView extends StatelessWidget {
+import '../../../core/utils/styles.dart';
+
+class CategoriesListView extends StatefulWidget {
   final List<ProductCategoryOrBrandModel> categoryItems;
 
   const CategoriesListView({super.key, required this.categoryItems});
 
+  @override
+  State<CategoriesListView> createState() => _CategoriesListViewState();
+}
+
+class _CategoriesListViewState extends State<CategoriesListView> {
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -16,20 +24,48 @@ class CategoriesListView extends StatelessWidget {
         physics: const BouncingScrollPhysics(),
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
-          return InkWell(
+          return CategoriesListviewItem(
             onTap: () {
               BlocProvider.of<ProductsCubit>(context)
-                  .filterProductsByCategory(categoryItems[index].name);
+                  .filterProductsByCategory(widget.categoryItems[index].name);
+              setState(() {
+                currentIndex = index;
+              });
             },
-            child: Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: Chip(
-                label: Text(categoryItems[index].name),
-              ),
-            ),
+            categoryItem: widget.categoryItems[index],
+            isSelected:  index ==currentIndex,
           );
         },
-        itemCount: categoryItems.length,
+        itemCount: widget.categoryItems.length,
+      ),
+    );
+  }
+}
+
+class CategoriesListviewItem extends StatelessWidget {
+  final Function()? onTap;
+  final ProductCategoryOrBrandModel categoryItem;
+  final bool isSelected;
+
+  const CategoriesListviewItem({
+    super.key,
+    required this.onTap,
+    required this.categoryItem,
+    required this.isSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Chip(
+          backgroundColor: isSelected ? AppStyles.kPrimaryColor : Colors.grey.withOpacity(.1),
+          label: Text(categoryItem.name , style: TextStyle(
+            color: isSelected ? Colors.white : Colors.black,
+          ),),
+        ),
       ),
     );
   }
