dd45d15dd39b0551edbf829ade1c119655346c27
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-06-04 11:11:14 +0300

    Validate TextFields

--- lib/presentation/screens/product_details_screen.dart
+++ lib/presentation/screens/product_details_screen.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:footwear_store_client/presentation/controller/products_cubit.dart';
+import 'package:footwear_store_client/presentation/controller/products_state.dart';
+import 'package:footwear_store_client/presentation/widgets/custom_snack_bar.dart';
 import 'package:http/http.dart' as http;
 import 'package:footwear_store_client/core/utils/styles.dart';
 import 'package:footwear_store_client/data/models/product_model.dart';
 import 'package:footwear_store_client/presentation/widgets/custom_text_field.dart';
+
+import '../../data/models/order_product_model.dart';
 
 class ProductDetailsScreen extends StatefulWidget {
   final ProductModel product;
@@ -59,6 +65,10 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
 
   @override
   Widget build(BuildContext context) {
+    var formKey = GlobalKey<FormState>();
+    var customerNameController = TextEditingController();
+    var phoneController = TextEditingController();
+    var addressController = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -68,89 +78,228 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              GestureDetector(
-                onTap: () => _validateAndDisplayImage(context),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10.0),
-                  child: Image.network(
-                    widget.product.imageUrl ??
-                        'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png',
-                    fit: BoxFit.contain,
-                    width: double.infinity,
-                    height: MediaQuery.sizeOf(context).height * .25,
-                    errorBuilder: (context, error, stackTrace) {
-                      // return const Text('Invalid URL format');
-                      return Center(
+      body: BlocConsumer<ProductsCubit, ProductsStates>(
+        listener: (context, state) {
+          if (state is CreateOrderSuccessState) {
+            CustomSnackBarOverlay.show(
+              context,
+              message: 'Success',
+              messageDescription:'Order Created Successfully',
+                msgColor: Colors.green
+            );
+            customerNameController.clear();
+            phoneController.clear();
+            addressController.clear();
+          }
+          if (state is CreateOrderFailureState) {
+            CustomSnackBarOverlay.show(
+              context,
+              message: 'Error',
+              messageDescription:'Order creation failed. Please try again.',
+              msgColor: Colors.red
+            );
+
+
+          }
+          if (state is StripeFailureState) {
+            CustomSnackBarOverlay.show(
+                context,
+                message: 'Error',
+                messageDescription:"Something went wrong. Please try again later.",
+                msgColor: Colors.red
+            );
+          }
+
+        },
+        builder: (context, state) {
+          var cubit = BlocProvider.of<ProductsCubit>(context);
+          return SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    GestureDetector(
+                      onTap: () => _validateAndDisplayImage(context),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10.0),
                         child: Image.network(
-                            height: 200,
-                            'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png'),
-                      );
-                    },
-                  ),
+                          widget.product.imageUrl ??
+                              'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png',
+                          fit: BoxFit.contain,
+                          width: double.infinity,
+                          height: MediaQuery.sizeOf(context).height * .25,
+                          errorBuilder: (context, error, stackTrace) {
+                            // return const Text('Invalid URL format');
+                            return Center(
+                              child: Image.network(
+                                  height: 200,
+                                  'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png'),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      widget.product.name,
+                      style: const TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 18),
+                    Text(widget.product.description),
+                    const SizedBox(height: 20),
+                    Text(
+                      'Price: ${widget.product.price}',
+                      style: const TextStyle(
+                        color: Colors.green,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    CustomTextField(
+                      hintText: 'Enter Your Name',
+                      labelText: 'Name',
+                      prefixIcon: Icons.person,
+                      keyboardType: TextInputType.name,
+                      controller: customerNameController,
+                      validator: (value) => validateTextFieldInput(
+                        value,
+                        errorMsg: 'Name Required ',
+                      ),
+                      onFieldSubmitted: (_) {
+                        // if (formKey.currentState!.validate()) {
+                        //   cubit.createPayment(
+                        //     amount: (widget.product.price).toInt(),
+                        //     currency: 'USD',
+                        //   );
+                        // }
+                      },
+                    ),
+                    const SizedBox(height: 4),
+                    CustomTextField(
+                      hintText: 'Enter Your Phone',
+                      labelText: 'Phone Number',
+                      prefixIcon: Icons.phone,
+                      keyboardType: TextInputType.phone,
+                      controller: phoneController,
+                      validator: (value) => validateTextFieldInput(
+                        value,
+                        errorMsg: 'Phone Number Required ',
+                      ),
+                      onFieldSubmitted: (_) {
+                        // if (formKey.currentState!.validate()) {
+                        //   cubit.createPayment(
+                        //     amount: (widget.product.price).toInt(),
+                        //     currency: 'USD',
+                        //   );
+                        // }
+                      },
+                    ),
+                    const SizedBox(height: 4),
+                    CustomTextField(
+                      maxLines: 2,
+                      hintText: 'Enter Your Billing Address',
+                      labelText: 'Billing Address',
+                      prefixIcon: Icons.home,
+                      keyboardType: TextInputType.text,
+                      controller: addressController,
+                      validator: (value) => validateTextFieldInput(
+                        value,
+                        errorMsg: 'Billing Address Required ',
+                      ),
+                      onFieldSubmitted: (_) {
+                        final orderProductModel = OrderProductModel(
+                          address: addressController.text,
+                          customer: customerNameController.text,
+                          productName: widget.product.name,
+                          phone: phoneController.text,
+                          price: widget.product.price.toString(),
+                          transactionId: '',
+                          // This will be updated after payment
+                          dateTime: DateTime.now().toIso8601String(),
+                        );
+                        cubit.createPayment(
+                          amount: widget.product.price.toInt(),
+                          currency: 'USD',
+                          orderProductModel: orderProductModel,
+                        );
+
+                      },
+                    ),
+                    const SizedBox(height: 10),
+                    if (state is StripeLoadingState)
+                      const Padding(
+                        padding: EdgeInsets.only(top: 20),
+                        child: Center(
+                          child: CircularProgressIndicator(
+                              color: AppStyles.kPrimaryColor),
+                        ),
+                      ),
+                    if (state is! StripeLoadingState)
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton.icon(
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: AppStyles.kPrimaryColor,
+                          ),
+                          onPressed: () {
+                            if (formKey.currentState!.validate()) {
+                              final orderProductModel = OrderProductModel(
+                                address: addressController.text,
+                                customer: customerNameController.text,
+                                productName: widget.product.name,
+                                phone: phoneController.text,
+                                price: widget.product.price.toString(),
+                                transactionId: '',
+                                // This will be updated after payment
+                                dateTime: DateTime.now().toIso8601String(),
+                              );
+                              cubit.createPayment(
+                                amount: widget.product.price.toInt(),
+                                currency: 'USD',
+                                orderProductModel: orderProductModel,
+                              );
+
+                            }
+                          },
+                          icon: const Icon(Icons.payment, size: 18),
+                          label: const Text(
+                            'Buy Now',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    if (_imageError != null)
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16.0),
+                        child: Text(
+                          _imageError!,
+                          style: const TextStyle(color: Colors.red),
+                        ),
+                      ),
+                  ],
                 ),
               ),
-              const SizedBox(height: 20),
-              Text(
-                widget.product.name,
-                style: const TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 18),
-              Text(widget.product.description),
-              const SizedBox(height: 20),
-              Text(
-                'Rs: ${widget.product.price}',
-                style: const TextStyle(
-                  color: Colors.green,
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 20),
-              const CustomTextField(
-                maxLines: 3,
-                hintText: 'Enter Your Billing Address',
-                labelText: 'Billing Address',
-              ),
-              const SizedBox(height: 10),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor: AppStyles.kPrimaryColor,
-                  ),
-                  onPressed: () {
-                    ///Add Payment METHOD !!
-                  },
-                  child: const Text(
-                    'Buy Now',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-              if (_imageError != null)
-                Padding(
-                  padding: const EdgeInsets.only(top: 16.0),
-                  child: Text(
-                    _imageError!,
-                    style: const TextStyle(color: Colors.red),
-                  ),
-                ),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
+}
+
+String? validateTextFieldInput(String? value, {required String errorMsg}) {
+  if (value?.isEmpty ?? true) {
+    return errorMsg;
+  }
+  return null;
 }
 
 class CustomBottomSheet extends StatelessWidget {
