3c0f337e6f35d445a10d0640b0b2e6446d122f59
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-06-04 11:11:32 +0300

    Add Order Collection

--- lib/const.dart
+++ lib/const.dart
@@ -1,3 +1,4 @@
 const String kProductsCollection = 'products' ;
 const String kProductsCategoryCollection = 'categories' ;
 const String kProductsBrandsCollection = 'brand' ;
+const String kProductsOrdersCollection = 'orders' ;
