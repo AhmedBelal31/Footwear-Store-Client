07af304c87ff91b11d28ab3f1179610abc2a3bf2
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-06-18 06:57:28 +0300

    Manage App Routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:footwear_store_client/core/modules/registration_modules/presentation/controller/auth_cubit.dart';
 import 'package:footwear_store_client/core/modules/registration_modules/presentation/screens/login_screen.dart';
+import 'package:footwear_store_client/core/modules/registration_modules/presentation/screens/register_screen.dart';
+import 'package:footwear_store_client/core/utils/styles.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'core/modules/home_module/presentation/controller/products_cubit.dart';
 import 'core/modules/home_module/presentation/screens/home_screen.dart';
+import 'core/modules/registration_modules/presentation/screens/reset_password_screen.dart';
 import 'core/services/stripe_keys.dart';
 import 'core/utils/bloc_observer.dart';
 
@@ -21,49 +26,60 @@ void main() async {
 //Cache Products Images
 // Logic For Home Screen => Sort , Filter Brand
 // Add Shimmer Effect For Home Screen
+// Send OTP
+// Add Payment Methods
+//Add On Boarding
+// Add State Of multiDropDownButton To work Without ALT+S
+// Solve Delete Last Item From Admin APP , Floating Action Button Covers Delete Icon
 
-/// Add Payment Methods
+/// Add OoBoarding
 /// Add Login , Register Validation
-/// Send OTP
 /// Save Token By Shared Pref , Get Storage , Search For Caching Tokens
 /// Add Email For Login
 /// LogOut From App
-/// Add Offer Percentage To Admin , And Calc Offer Price
-/// Add State Of multiDropDownButton To work Without ALT+S
-
 /// Record Video For APPS
-/// Solve Delete Last Item From Admin APP , Floating Action Button Covers Delete Icon
 /// Firebase Login Rules -> Check IF Admin -> Footwear Admin App
 ///                               IF User -> Footwear Client App
-
 
 class FootWearStoreClient extends StatelessWidget {
   const FootWearStoreClient({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => ProductsCubit()..fetchAllProducts()..fetchAllProductsCategories(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => ProductsCubit()
+            ..fetchAllProducts()
+            ..fetchAllProductsCategories(),
+        ),
+        BlocProvider(
+          create: (context) => AuthCubit(FirebaseAuth.instance),
+        ),
+      ],
       child: MaterialApp(
-        title: 'FootWear Store Client',
+        title: 'ShoeHub',
         theme: ThemeData(
           // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
           textTheme: GoogleFonts.almaraiTextTheme(
-            Theme
-                .of(context)
-                .textTheme,
+            Theme.of(context).textTheme,
           ),
           scaffoldBackgroundColor: Colors.white,
-          appBarTheme: const AppBarTheme(
-              backgroundColor: Colors.white
-          ),
+          appBarTheme: const AppBarTheme(backgroundColor: Colors.white),
+
         ),
         debugShowCheckedModeBanner: false,
-        home: const LoginScreen(),
+        initialRoute:LoginScreen.screenRoute,
+        routes: {
+          LoginScreen.screenRoute: (context) => LoginScreen(),
+          '/reset_password': (context) => ResetPasswordScreen(),
+          RegisterScreen.screenRoute: (context) => RegisterScreen(),
+          HomeScreen.screenRoute: (context) => HomeScreen(),
+        },
+
+
       ),
     );
   }
 }
-
-
