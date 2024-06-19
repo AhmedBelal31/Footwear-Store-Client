62b79c1d137633af7d9dc7c3f9570dffb56090ff
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-06-19 03:29:12 +0300

    Change Login Screen UI

--- lib/core/modules/registration_modules/presentation/screens/login_screen.dart
+++ lib/core/modules/registration_modules/presentation/screens/login_screen.dart
@@ -1,65 +1,264 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:footwear_store_client/core/modules/registration_modules/presentation/controller/auth_cubit.dart';
+// import 'package:footwear_store_client/core/utils/styles.dart';
+// import '../../../../utils/widgets/custom_text_field.dart';
+// import '../../../home_module/presentation/screens/home_screen.dart';
+// import '../functions/validate_email.dart';
+// import '../functions/validate_password.dart';
+// import 'register_screen.dart';
+//
+// class LoginScreen extends StatefulWidget {
+//   const LoginScreen({super.key});
+//
+//   static const screenRoute = 'loginScreen';
+//
+//   @override
+//   State<LoginScreen> createState() => _LoginScreenState();
+// }
+//
+// class _LoginScreenState extends State<LoginScreen> {
+//   var _formKey = GlobalKey<FormState>();
+//
+//   final TextEditingController _emailController = TextEditingController();
+//   final TextEditingController _passwordController = TextEditingController();
+//
+//   AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+//
+//   @override
+//   void dispose() {
+//     _emailController.dispose();
+//     _passwordController.dispose();
+//     super.dispose();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: BlocBuilder<AuthCubit, AuthStates>(
+//         builder: (context, state) {
+//           return Form(
+//             key: _formKey,
+//             child: Padding(
+//               padding: const EdgeInsets.all(8.0),
+//               child: Container(
+//                 width: double.infinity,
+//                 padding: const EdgeInsets.all(20),
+//                 decoration: BoxDecoration(
+//                     //color: Colors.blueGrey[50],
+//                     ),
+//                 child: Column(
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   children: [
+//                     const Text(
+//                       'Welcome Back !',
+//                       style: TextStyle(
+//                           fontSize: 28,
+//                           fontWeight: FontWeight.bold,
+//                           color: AppStyles.kPrimaryColor),
+//                     ),
+//                     const SizedBox(height: 30),
+//                     CustomTextField(
+//                       labelText: 'Email-Address',
+//                       hintText: 'Enter Your Email-Address ',
+//                       prefixIcon: Icons.email,
+//                       controller: _emailController,
+//                       autovalidateMode: autovalidateMode,
+//                       validator: validateEmail,
+//                     ),
+//                     CustomTextField(
+//                       labelText: 'Password',
+//                       hintText: 'Enter Your Password ',
+//                       controller: _passwordController,
+//                       prefixIcon: Icons.lock,
+//                       autovalidateMode: autovalidateMode,
+//                       obscureText: context.read<AuthCubit>().isPasswordShown,
+//                       suffixIcon: IconButton(
+//                         onPressed: () {
+//                           context.read<AuthCubit>().changePasswordIcon();
+//                         },
+//                         icon: Icon(context.read<AuthCubit>().passwordIcon),
+//                       ),
+//                       keyboardType: TextInputType.visiblePassword,
+//                       validator: validatePassword,
+//                     ),
+//                     const SizedBox(height: 20),
+//                     SizedBox(
+//                       width: double.infinity,
+//                       child: ElevatedButton(
+//                         style: ElevatedButton.styleFrom(
+//                             foregroundColor: Colors.white,
+//                             backgroundColor: AppStyles.kPrimaryColor,
+//                             shape: RoundedRectangleBorder(
+//                               borderRadius: BorderRadius.circular(12),
+//                             )),
+//                         onPressed: () {
+//                           validateForm(context);
+//                         },
+//                         child: const Text('Login'),
+//                       ),
+//                     ),
+//                     TextButton(
+//                       onPressed: () {
+//                         Navigator.of(context).push(
+//                           MaterialPageRoute(
+//                             builder: (context) => RegisterScreen(),
+//                           ),
+//                         );
+//                       },
+//                       child: const Text('Register new account ?'),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             ),
+//           );
+//         },
+//       ),
+//     );
+//   }
+//
+//   void validateForm(BuildContext context) {
+//     if (_formKey.currentState?.validate() ?? false) {
+//       // Navigator.of(context).pushNamed(HomeScreen.screenRoute);
+//       ScaffoldMessenger.of(context)
+//           .showSnackBar(SnackBar(content: Text('Hello Body')));
+//     } else {
+//       setState(() {
+//         autovalidateMode = AutovalidateMode.always;
+//       });
+//     }
+//   }
+// }
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:footwear_store_client/core/modules/registration_modules/presentation/controller/auth_cubit.dart';
 import 'package:footwear_store_client/core/utils/styles.dart';
 import '../../../../utils/widgets/custom_text_field.dart';
 import '../../../home_module/presentation/screens/home_screen.dart';
+import '../functions/validate_email.dart';
+import '../functions/validate_password.dart';
 import 'register_screen.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  static const screenRoute = 'loginScreen';
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  var _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        padding: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: Colors.blueGrey[50],
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Welcome Back !',
-              style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  color: AppStyles.kPrimaryColor),
-            ),
-            const SizedBox(height: 30),
-            const CustomTextField(
-              labelText: 'Mobile Number',
-              hintText: 'Enter Your Mobile Number ',
-              prefixIcon: Icons.phone_android,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: AppStyles.kPrimaryColor,
+      body: BlocBuilder<AuthCubit, AuthStates>(
+        builder: (context, state) {
+          return Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  //color: Colors.blueGrey[50],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Welcome Back !',
+                      style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                          color: AppStyles.kPrimaryColor),
+                    ),
+                    const SizedBox(height: 30),
+                    CustomTextField(
+                      labelText: 'Email-Address',
+                      hintText: 'Enter Your Email-Address ',
+                      prefixIcon: Icons.email,
+                      controller: _emailController,
+                      autovalidateMode: autovalidateMode,
+                      validator: validateEmail,
+                    ),
+                    CustomTextField(
+                      labelText: 'Password',
+                      hintText: 'Enter Your Password ',
+                      controller: _passwordController,
+                      prefixIcon: Icons.lock,
+                      autovalidateMode: autovalidateMode,
+                      obscureText: context.read<AuthCubit>().isLoginPasswordShown,
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          context.read<AuthCubit>().changeLoginPasswordIcon();
+                        },
+                        icon: Icon(context.read<AuthCubit>().loginPasswordIcon),
+                      ),
+                      keyboardType: TextInputType.visiblePassword,
+                      validator: validatePassword,
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: AppStyles.kPrimaryColor,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            )),
+                        onPressed: () {
+                          validateForm(context);
+                        },
+                        child: const Text('Login'),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => RegisterScreen(),
+                          ),
+                        );
+                      },
+                      child: const Text('Register new account ?'),
+                    ),
+                  ],
+                ),
               ),
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => HomeScreen(),
-                  ),
-                );
-              },
-              child: const Text('  Login  '),
             ),
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => RegisterScreen(),
-                  ),
-                );
-              },
-              child: const Text('Register new account ?'),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
+  }
+
+  void validateForm(BuildContext context) {
+    if (_formKey.currentState?.validate() ?? false) {
+      // Navigator.of(context).pushNamed(HomeScreen.screenRoute);
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Hello Body')));
+    } else {
+      setState(() {
+        autovalidateMode = AutovalidateMode.always;
+      });
+    }
   }
 }
